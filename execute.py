fe07e18a80950b51ea8401d3d1ee5eb091254742
Jamie Eadie <j.a.eadie@dundee.ac.uk>
2014-02-19 14:55:05 +0000

    half working script to execute html form search

---
+++ execute.py
@@ -0,0 +1,31 @@
+# -*- coding: iso-8859-1 -*-
+#import database query script
+
+
+import models
+
+Gene = models.Gene(%s)
+
+
+#import cgi programs
+
+import cgi
+import cgitb
+
+cgitb.enable()
+
+
+form = cgi.FieldStorage()
+
+print "content-type: text/html" #html stuff is following
+print
+print "<html><head> <title> Your results </title></head>"
+print "<body><h1> Results</h1>"
+print "<table><tr><th>Key</th><th>Value</th></tr>"
+
+for k in form.keys():
+    print "<tr><td>%s</td><td>%s</td></tr>"%(k, form[k])
+
+print "<table>"
+
+print "</body></html>"
